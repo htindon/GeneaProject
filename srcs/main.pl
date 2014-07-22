923d81c3a56cf16c8f8139e78d97414a7ec50acf
Helene Tindon <htindon@student.42.fr>
2014-07-22 15:01:43 +0200

    first debugging ^^

--- srcs/main.pl
+++ srcs/main.pl
@@ -6,7 +6,7 @@
 #    By: htindon <htindon@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2014/07/22 14:30:24 by htindon           #+#    #+#              #
-#    Updated: 2014/07/22 14:56:54 by htindon          ###   ########.fr        #
+#    Updated: 2014/07/22 15:01:26 by htindon          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,14 +14,15 @@ use strict;
 use warnings;
 
 sub open_file {
-	my ($mode, $filename) =  @_;
+	my $mode = $_[0];
+	my $filename =  $_[1];
 	open (my $fh, $mode, $filename)
 		|| die "Error, cannot open $mode $filename: $!";
 	return ($fh);
 }
 
 sub	print_data {
-	my $filename = @_;
+	my $filename = $_[0];
 	my $fh = open_file("<", $filename);
 	while ( my $line = <$fh> ) {
 		print $line;
